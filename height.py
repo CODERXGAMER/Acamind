62d402dd5d1df19f3278564b8806b7403f25cd6b
Emil <110276126+CODERXGAMER@users.noreply.github.com>
2024-08-31 16:19:44 +0400

    Add files via upload

---
+++ height.py
@@ -0,0 +1,7 @@
+height_cm = int(input("Enter height in centimeters: "))
+
+inches = height_cm * 0.394
+feet = height_cm * 0.0328
+
+print("The length in inches : ", inches)
+print("The length in feet : ", feet)
